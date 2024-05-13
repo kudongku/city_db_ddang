0df4fbe4601c721ee7ff64410735ddcaae488bda
kudongku <kudongku@naver.com>
2024-05-14 00:26:14 +0900

    :art: (global) : git init commit

---
+++ src/main/java/com/example/city_db_ddang/CityDbDdangApplication.java
@@ -0,0 +1,13 @@
+package com.example.city_db_ddang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityDbDdangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityDbDdangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/city_db_ddang/CityDbDdangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.city_db_ddang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityDbDdangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
