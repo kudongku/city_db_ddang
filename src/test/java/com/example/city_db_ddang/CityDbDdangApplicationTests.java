e1e29cce97caec2214f3948907e50c7aee2401fe
kudongku <kudongku@naver.com>
2024-05-14 01:42:49 +0900

    :recycle: (global) : test 순서 지정

--- src/test/java/com/example/city_db_ddang/CityDbDdangApplicationTests.java
+++ src/test/java/com/example/city_db_ddang/CityDbDdangApplicationTests.java
@@ -1,15 +1,19 @@
 package com.example.city_db_ddang;
 
 import com.example.city_db_ddang.repository.AdminDistrictRepository;
-import com.example.city_db_ddang.service.ExcelReadService;
+import com.example.city_db_ddang.service.AdminDistrictService;
 import com.example.city_db_ddang.service.TownService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 class CityDbDdangApplicationTests {
 
@@ -20,20 +24,23 @@ class CityDbDdangApplicationTests {
     TownService townService;
 
     @Test
+    @Order(1)
     void contextLoads() {
     }
 
     @Test
+    @Order(2)
     @Rollback(false)
     @DisplayName("townList 엑셀 파일 DB에 저장")
     void saveTownListDB() {
         //given
-        ExcelReadService excelReadService = new ExcelReadService(adminDistrictRepository);
+        AdminDistrictService adminDistrictService = new AdminDistrictService(adminDistrictRepository);
         //when,then
-        excelReadService.createAdminDistrict();
+        adminDistrictService.createAdminDistrict();
     }
 
     @Test
+    @Order(3)
     @Rollback(false)
     @DisplayName("town DB에 저장")
     void saveTownDB() throws JsonProcessingException {
